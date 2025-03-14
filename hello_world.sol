b3ba265f30ab47264a14f4ba25afdd391f81791b
snehasharma76 <snehasharma7620@gmail.com>
2025-03-14 23:35:39 +0530

    Initial contracts of the 30day Building with Solidity Campaign

---
+++ hello_world.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract hello_world{
+
+    function get_message(string memory message) public pure returns(string memory){
+        return message;
+    }
+}
